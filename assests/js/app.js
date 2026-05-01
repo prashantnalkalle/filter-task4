481c41becebbb4d79a76935be54964c11fdbbccf
PRASHANT NALKALLE <pnalkalle@gmail.com>
2026-05-01 11:10:26 +0530

    work on functionality

--- assests/js/app.js
+++ assests/js/app.js
@@ -6,6 +6,8 @@ const sidebar = document.getElementById("sidebar")
 const overlay = document.getElementById("overlay")
 const selectcat = [...document.querySelectorAll('.sidebar div')]
 const selectall = document.querySelector('.selectall')
+const images = document.querySelectorAll(".images img");
+
 
 function oncatechange(e){
     let selectmovie = e.target.value
@@ -44,9 +46,39 @@ function forall(){
      const all =[...document.querySelectorAll('.all')]
     all.forEach(ele => ele.classList.remove('d-none'))
 }
+
+
+
+function onenterhandl(eve){
+    images.forEach(i=>{
+        if(i!==eve.target){
+            i.style.filter ='blur(5px)';
+            i.style.transform ="scale(.9)";
+            i.style.transition = " all .3s ease-in-out";
+        }else{
+            i.style.transform = 'scale(1.1)';
+            i.style.transition = " all .3s linear";
+            
+        }
+    })
+}
+
+function onleavehandl(eve){
+    images.forEach(i=>{
+        i.style.filter = 'blur(0)';
+        i.style.transform = 'scale(1)';
+    })
+}
+
 close1.addEventListener("click",onclick)
 icon.addEventListener("click",onclick)
 overlay.addEventListener("click",onclick) 
 movie.addEventListener('change',oncatechange)
 selectcat.forEach(ele => ele.addEventListener('click',onselect))
 selectall.addEventListener('click',forall)
+
+
+images.forEach(img =>{
+    img.addEventListener("mouseenter",onenterhandl)
+    img.addEventListener('mouseleave',onleavehandl)
+})
